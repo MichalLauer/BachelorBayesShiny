ca55af4c36824626ff4433eb89d65e0ec0531e01
Michal Lauer <michal.lauer.25@gmail.com>
2025-01-12 01:56:50 +0100

    Oprava neparametrického výpočtu

--- R/wilcox_test_stat.R
+++ R/wilcox_test_stat.R
@@ -1,24 +1,21 @@
 wilcox_test_stat <- function(test, c, s) {
+  W <- test$statistic
+
+  # Jednovýběrový test - vše je v pořádku
   if (is.null(s$x2)) {
-    W <- test$statistic
-    mu <- c$n*(c$n + 1)/4
-    s2 <- c$n*(c$n + 1)*(2*c$n + 1)/24
+    n_non <- sum(s$x1 != c$H0)
+    mu <- n_non * (n_non + 1) / 4
+    s2 <- n_non * (n_non + 1) * (2 * n_non + 1) / 24
   } else {
     if (c$paired) {
-      d <- s$x1 - s$x2
-      d_nonzero <- d[d != 0]
-      n <- length(d_nonzero)
-      ranks <- rank(abs(d_nonzero))
-      signed_ranks <- ranks * sign(d_nonzero)
-      W <- sum(signed_ranks[signed_ranks > 0])
-      mu <- c$n * (c$n + 1) / 4
-      s2 <- c$n * (c$n + 1) * (2 * c$n + 1) / 24
+      n_non <- sum((s$x1 - s$x2) != c$H0)
+      mu <- n_non * (n_non + 1) / 4
+      s2 <- n_non * (n_non + 1) * (2 * n_non + 1) / 24
     } else {
-      W <- test$statistic + c$n * (c$n + 1) / 2
-      mu <- c$n*(2*c$n + 1)/2
-      s2 <- c$n*c$n*(2*c$n + 1)/12
+      mu <- (c$n * c$n) / 2
+      s2 <- c$n * c$n * (c$n + c$n + 1) / 12
     }
   }
 
-  (W - mu) / sqrt(s2)
+  return( (W - mu) / sqrt(s2) )
 }
